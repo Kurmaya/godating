69d7be679eb1db273d04ce51161994a1cf762c94
Kurmaya <102325773+Kurmaya@users.noreply.github.com>
2024-03-23 04:45:10 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,22 @@
 
+const navbar = document.querySelector('nav');
+//navbar resize on scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    // navbar.style.height = '10vh';
+    navbar.classList.add('active');
+    document.querySelector(".logo").classList.add('active');
+  } else {
+    // navbar.style.padding = "80px 10px";
+    
+    navbar.classList.remove('active');
+    document.querySelector(".logo").classList.remove('active');
+  }
+}
+
+
 const one = document.querySelector('.head-one'),
 two =document.querySelector('.head-two'),
 three =document.querySelector('.head-three'),
@@ -11,13 +29,17 @@ const drop = document.querySelector('.dots');
 const close = document.querySelector('.close');
 drop.addEventListener('click',function(){
     document.querySelector('.nav-drop').classList.add('active');
+    drop.style.opacity='0';
     document.body.style.overflow='hidden';
 })
 close.addEventListener('click',function(){
     document.querySelector('.nav-drop').classList.remove('active');
+    drop.style.opacity='1';
     document.body.style.overflowY='scroll';
 })
 gsap.registerPlugin(ScrollTrigger);
+
+//hero section animation
 let tl = gsap.timeline({repeat:-1});
 tl.to(iHolder,{
     left: '45%',
@@ -85,72 +107,6 @@ tl.to(three,{
     
 })
 
-// tl.to(iHolder,{
-//     left: '40%',
-//     duration:1,
-//     ease: "elastic.out(.2,0.3)",
-// },'simultaneously')
-// tl.to(one,{
-//     left:'10%',
-//     duration:.5,
-//     ease: "elastic.out(.2,0.3)",
-// },'simultaneously')
-// tl.to(iHolder,{
-//     left:'125%',
-//     duration:.2,
-//     delay:3,
-// })
-// tl.to(one,{
-//     left:'-200%',
-//     duration:.2,  
-// })
-// tl.to(iTwo,{
-//     zIndex:'5',
-// })
-// tl.to(iHolder,{
-//     left: '40%',
-//     duration:.8,
-//     ease: "elastic.out(.2,0.3)",
-// })
-// tl.to(two,{
-//     left:'10%',
-//     duration:.5,
-//     ease: "elastic.out(.2,0.3)",
-// })
-// tl.to(iHolder,{
-//     left:'125%',
-//     duration:.2,
-//     delay:2,
-// })
-// tl.to(two,{
-//     left:'-200%',
-//     duration:.2,
-    
-// })
-// tl.to(iThree,{
-//     zIndex:'5',
-// })
-// tl.to(iHolder,{
-//     left: '40%',
-//     duration:.8,
-//     ease: "elastic.out(.2,0.3)",
-// })
-// tl.to(three,{
-//     left:'10%',
-//     duration:.3,
-//     ease: "elastic.out(.2,0.3)",
-// })
-// tl.to(iHolder,{
-//     left: '125%',
-//     duration:.2,
-//     delay:3,
-// })
-// tl.to(three,{
-//     left:'-200%',
-//     duration:.2,
-    
-// })
-
 // animation for what section
 const heroY = document.querySelector('.hero').getBoundingClientRect().height;
 const heroX = document.querySelector('.hero').getBoundingClientRect().width;
@@ -203,9 +159,9 @@ tl3.to('.red-circle',{
 circles.forEach((circle,i)=>{
     tl3.from(circle,{
         left:'150%',
-        duration:.2,
+        duration:.1,
         ease: "power1",
-        stagger:0.2,
+        // stagger:0.05,
     })
 });
 // animation for how to section
@@ -221,27 +177,25 @@ let tl4 = gsap.timeline({
         toggleActions: 'play none play reverse'
       }
 })
+tl4.from('#howto h2',{
+    x:'-100vw',
+    opacity:0,
+    ease: "elastic.out(.2,0.3)",
+})
 tl4.to('.black-circle',{
     y: '+=700',
-    x: '-105vw',
+    x: '-110vw',
 },'simultaneously')
 tl4.to('.red-circle',{
-    y:'+=500',
-    x:'90vw',
+    y:'+=700',
+    x:'105vw',
 },'simultaneously')
-let navbar = document.querySelector('nav');
 
-window.onscroll = function() {scrollFunction()};
+tl4.from('.howto-img-container',{
+    y:'200vh',
+    opacity:0,
+    ease: "elastic.out(.2,0.3)",
+})
 
-function scrollFunction() {
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    // navbar.style.height = '10vh';
-    navbar.classList.add('active');
-    document.querySelector(".logo").classList.add('active');
-  } else {
-    // navbar.style.padding = "80px 10px";
-    
-    navbar.classList.remove('active');
-    document.querySelector(".logo").classList.remove('active');
-  }
-}
+const phone= document.querySelector('.howto-img-container');
+
